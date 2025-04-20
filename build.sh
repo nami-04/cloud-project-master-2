55d759462c422669e697c32dad302363cb22171b
nami-04 <namithar2004@gmail.com>
2025-04-20 14:57:04 +0530

    Update build configuration for Render deployment

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install system dependencies
-apt-get update
-apt-get install -y python3-dev gcc
-
 # Install Python packages
 pip install --upgrade pip
 pip install -r requirements.txt
