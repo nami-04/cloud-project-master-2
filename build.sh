8eda94d11cead933990a5bf16c3ae53dd401d21e
nami-04 <namithar2004@gmail.com>
2025-04-20 15:33:26 +0530

    Update package versions and build process for better compatibility

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 # Install Python packages
 pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt --no-cache-dir
+pip install --no-cache-dir -r requirements.txt
 
 # Collect static files and run migrations
 python manage.py collectstatic --no-input
