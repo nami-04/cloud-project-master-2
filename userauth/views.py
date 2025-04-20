17838c8333924ab193e70ff2ca744dd13f7a3c13
nami-04 <namithar2004@gmail.com>
2025-04-20 16:07:15 +0530

    Update userauth views to use environment variables for Firebase

--- userauth/views.py
+++ userauth/views.py
@@ -7,9 +7,21 @@ from club import datahandler as clubdata
 from django.http import *
 import traceback
 import pyrebase
+import os
+from dotenv import load_dotenv
 
-#enter the firebase config api keys
+load_dotenv()
+
+# Firebase configuration from environment variables
 firebaseConfig = {
+    "apiKey": os.getenv('FIREBASE_API_KEY'),
+    "authDomain": os.getenv('FIREBASE_AUTH_DOMAIN'),
+    "databaseURL": os.getenv('FIREBASE_DATABASE_URL'),
+    "projectId": os.getenv('FIREBASE_PROJECT_ID'),
+    "storageBucket": os.getenv('FIREBASE_STORAGE_BUCKET'),
+    "messagingSenderId": os.getenv('FIREBASE_MESSAGING_SENDER_ID'),
+    "appId": os.getenv('FIREBASE_APP_ID'),
+    "measurementId": os.getenv('FIREBASE_MEASUREMENT_ID')
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
@@ -48,7 +60,7 @@ def loginuser(request):
                 raise Exception
         except:
             traceback.print_exc()
-            return render(request, 'login.html' , {"alert" : 1})
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
     
     return render(request, 'login.html' , {"alert" : 0})
 
