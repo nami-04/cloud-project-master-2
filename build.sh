38329b84ed837f82aba4fc8e6c1752c8838a9694
nami-04 <namithar2004@gmail.com>
2025-04-20 14:55:42 +0530

    Update build script to handle system dependencies and package installation

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# Install system dependencies
+apt-get update
+apt-get install -y python3-dev gcc
+
+# Install Python packages
+pip install --upgrade pip
 pip install -r requirements.txt
 
+# Collect static files and run migrations
 python manage.py collectstatic --no-input
 python manage.py migrate 
